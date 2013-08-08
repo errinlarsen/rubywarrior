27c6db307f7b2829669abd22b39cce627635580f
Errin Larsen <elarsen@insightcards.com>
2013-08-08 15:21:20 -0500

    Clean up failure messages and other stuff

--- test/irken1-beginner/acceptance_test.rb
+++ test/irken1-beginner/acceptance_test.rb
@@ -4,15 +4,15 @@ require "ruby_warrior"
 
 class AcceptanceTest < Minitest::Test
   def setup
-    cli_arguments = ["-d", "rubywarrior/irken1-beginner", "-s", "-t", "0"]
     @stdin = StringIO.new
     @stdout = StringIO.new
+    cli_arguments = %w[-d rubywarrior/irken1-beginner -s -t 0]
     @runner = RubyWarrior::Runner.new(cli_arguments, @stdin, @stdout)
   end
 
   def test_level_1
     @runner.run
-    puts "STDOUT ==> #{@stdout.string}"
-    assert_includes @stdout.string, "Success! You have found the stairs."
+    success_msg = "Success! You have found the stairs."
+    assert_includes @stdout.string, success_msg, stdout_failure_msg(@stdout)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,7 @@
 require "minitest/autorun"
 require "rr"
+
+def stdout_failure_msg(stdout)
+  formatted = stdout.string.gsub(/^/, " " * 4).gsub(/\.\n\Z/, '')
+  return "Output captured:\n#{formatted}"
+end
