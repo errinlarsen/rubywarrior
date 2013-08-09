59cc86c775139a5a6c842c4a487b60b8acce67d2
Errin Larsen <elarsen@insightcards.com>
2013-08-09 14:46:24 -0500

    [Lvl2] Create RubyWarriorRunner Mixin; Add custom Assertions; Clean up AcceptanceTest

--- test/irken1-beginner/level2/acceptance_test.rb
+++ test/irken1-beginner/level2/acceptance_test.rb
@@ -1,76 +1,38 @@
 require "test/test_helper"
-require "stringio"
-require "ruby_warrior"
+require "test/ruby_warrior_runner"
 
 class AcceptanceTest < Minitest::Test
+  include RubyWarriorRunner
+
   def setup
-    @stdin = StringIO.new
-    @stdout = StringIO.new
-    cli_arguments = %w[-d rubywarrior/irken1-beginner -s -t 0]
-    @runner = RubyWarrior::Runner.new(cli_arguments, @stdin, @stdout)
+    run_ruby_warrior
   end
 
   def test_level_is_successful
-    @runner.run
-    success_msg = "Success! You have found the stairs."
-    assert_includes @stdout.string, success_msg, stdout_failure_msg(@stdout)
+    assert_ruby_warrior_output_includes "Success! You have found the stairs."
   end
 
   def test_player_does_not_die
-    @runner.run
-    death_msg = "irken1 dies"
-    refute_includes @stdout.string, death_msg, stdout_failure_msg(@stdout)
+    refute_ruby_warrior_output_includes "irken1 dies"
   end
 
   def test_enemy_dies
-    @runner.run
-    death_msg = "Sludge dies"
-    assert_includes @stdout.string, death_msg, stdout_failure_msg(@stdout)
+    assert_ruby_warrior_output_includes "Sludge dies"
   end
 
   def test_gained_best_level_score
-    @runner.run
-    assert best_level_score <= level_score(@stdout)
+    assert_ruby_warrior_level_score_meets_or_beats 12
   end
 
   def test_gained_best_time_bonus
-    @runner.run
-    assert best_time_bonus <= level_score(@stdout)
+    assert_ruby_warrior_time_bonus_meets_or_beats 10
   end
 
   def test_gained_best_clear_bonus
-    @runner.run
-    assert best_clear_bonus <= clear_bonus(@stdout)
+    assert_ruby_warrior_clear_bonus_meets_or_beats 4
   end
 
   def test_gained_best_total_score
-    @runner.run
-    assert best_total_score <= total_score(@stdout)
-  end
-
-
-  private
-  def best_level_score; return 12; end
-  def level_score(string_io)
-    m = /^Level Score: (?<score>\d+)$/.match(string_io.string)
-    return m[:score].to_i
-  end
-
-  def best_time_bonus;  return 10; end
-  def time_bonus(string_io)
-    m = /^Time Bonus: (?<bonus>\d+)$/.match(string_io.string)
-    return m[:bonus].to_i
-  end
-
-  def best_clear_bonus; return  4; end
-  def clear_bonus(string_io)
-    m = /^Clear Bonus: (?<bonus>\d+)$/.match(string_io.string)
-    return m[:bonus].to_i
-  end
-
-  def best_total_score; return 36; end
-  def total_score(string_io)
-    m = /^Total Score.*= (?<score>\d+)$/.match(string_io.string)
-    return m[:score].to_i
+    assert_ruby_warrior_total_score_meets_or_beats 36
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,2 @@
 require "minitest/autorun"
 require "rr"
-
-def stdout_failure_msg(stdout)
-  formatted = stdout.string.gsub(/^/, " " * 4).gsub(/\.\n\Z/, '')
-  return "Output captured:\n#{formatted}"
-end
